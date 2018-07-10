6f9b201991621730962fa22c4209d3ac1d40b334
Usman ali <34685892+usmanali08@users.noreply.github.com>
2018-07-10 11:47:09 +0530

    Add files via upload
    To declare variables and its values.

---
+++ variables.js
@@ -0,0 +1,15 @@
+//declaring variables
+var myName;
+//declaring values to the variables
+myName = "usman";
+
+//declaring variables and values in one step
+var myName = "usman";
+
+//other types of variable key words
+/*
+let (new variables can update old one)
+const(the variables remains constant)
+var(common keyword)
+*/
+console.log(myName); //printing the variables to te console.
