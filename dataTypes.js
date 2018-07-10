27613469218ffa690b82405250d4fe111d88e5da
usmanali08 <usmanali08@protonmail.com>
2018-07-10 17:15:15 +0100

    boolean with if else statement.

---
+++ dataTypes.js
@@ -0,0 +1,16 @@
+//boolean
+var data=true;
+if(data){
+console.log("boolean's rule");
+}
+else{
+    console.log("not a boolean");
+}
+//if boolean is false
+var data=false;
+if(data){
+console.log("boolean's rule");
+}
+else{
+    console.log("not a boolean");
+}
