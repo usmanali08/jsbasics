2355fcfd9e8d54e0906b7836aa74c104716559be
usmanali08 <usmanali08@protonmail.com>
2018-07-10 17:38:06 +0100

    data types

--- dataTypes.js
+++ dataTypes.js
@@ -1,16 +1,41 @@
 //boolean
-var data=true;
-if(data){
-console.log("boolean's rule");
-}
-else{
-    console.log("not a boolean");
+var data = true;
+if (data) {
+  console.log("boolean's rule");
+} else {
+  console.log("not a boolean");
 }
 //if boolean is false
-var data=false;
-if(data){
-console.log("boolean's rule");
+var data = false;
+if (data) {
+  console.log("boolean's rule");
+} else {
+  console.log("not a boolean");
 }
-else{
-    console.log("not a boolean");
-}
+//null
+var n = null;
+console.log(n * 32);
+
+//undefined datatype
+var n;
+console.log(n * 2);
+
+//string
+var myName = "usman";
+console.log(myName);
+
+//numbers
+var a = 2.1;
+var b = 3.4;
+console.log(a + b);
+
+//objects
+
+var mybike = newobject();
+newobject.make = "yamaha";
+newobject.model = "r15";
+
+//symbols
+var sym1 = symbol("usman");
+var sym2 = symbol("usman");
+console.log(sym1 === sym2);
